0e495dc83db641acbe933fae076a6c148cbe9c18
itachini2003 <ignacio.tachini@correo.ucu.edu.uy>
2023-10-23 08:55:11 -0300

    .

--- api-project/src/index.ts
+++ api-project/src/index.ts
@@ -1,19 +1,18 @@
-import express from 'express';
+import express from 'express'
+import gameActivity from './routes/gameActivities'
 
-import gameRoomsRouter from './routes/gameRooms';
+const app = express()
+app.use(express.json())
 
-const app = express();
-app.use(express.json());
-
-const PORT = 3000;
+const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('someone pinged here! ' + new Date().toLocaleDateString());
-    res.send('pong');
-});
+  console.log('someone pinged here! ' + new Date().toLocaleDateString())
+  res.send('pong')
+})
 
-app.use('/api/', gameRoomsRouter);
+app.use('/api', gameActivity)
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- api-project/src/routes/auth.ts
+++ api-project/src/routes/auth.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
 // Crea una nueva propuesta.
 router.post('/auth/login', (_req, _res) => {
-    
-});
+
+})
 
 router.post('/auth/refresh-token', (_req, _res) => {
-    
-});
 
-export default router;
+})
+
+export default router
